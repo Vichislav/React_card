33b2ce93a8aa5d13a32ca2b80690b0fc016869cc
Svalyan <you@example.com>
2022-09-15 23:44:05 +0600

    pre deploy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Container, Nav, Navbar} from "react-bootstrap";
 import logo from '../Assets/logo192.png'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../Pages/Home"
 import About from "../Pages/About"
 import Contacts from "../Pages/Contacts";
